54ca5b68ec4df6966f47a10ff27d54a617ae299c
ilkerkurtulan97 <ilker.kurtulan@hotmail.com>
2020-12-30 00:15:28 +0000

    First commit with major changes on odev.js file. Created dom elements and added dynamic selection to html via filtering the data.

--- odev.js
+++ odev.js
@@ -1,5 +1,5 @@
-import data from "./data.js"
-import {createTableElements} from "./main.js";
+import data from "./data.js";
+import { createTableElements } from "./main.js";
 
 /*
   ALWAYS USE IMPORTED data ARRAY TO MAKE MANIPULATIONS
@@ -27,10 +27,98 @@ import {createTableElements} from "./main.js";
 
 /* RESET ACTION */
 document.querySelector("#reset").addEventListener("click", () => {
-    createTableElements(data, "allcities");
-    createTableElements([], "singlecity")
+  createTableElements(data, "allcities");
+  createTableElements([], "singlecity");
 });
 
 /* START CODING HERE */
+
+// Button declerations
+
+let populationBiggerButton = document.getElementById("populationBigger");
+let landAreaLessButton = document.getElementById("landAreaLess");
+let doesAnyCityButton = document.getElementById("isPopulationLess");
+let doesEveryCityButton = document.getElementById("isLandBigger");
+let citySelect = document.getElementById("selectCity");
+let resetButton = document.getElementById("reset");
+
+
+
+//Table declerations
+
+let tableFiltered = document.getElementById("allcities");
+let tableFoundItem = document.getElementById("singlecity");
+
+
+populationBiggerButton.addEventListener("click", function () {
+  let populationBiggerthan = data.filter((city) => {
+    return city.population > 500000;
+  });
+  console.log(populationBiggerthan);
+  createTableElements(populationBiggerthan, "allcities");
+  createTableElements([], "singlecity");
+});
+
+landAreaLessButton.addEventListener("click", function () {
+  let areaLessThan = data.filter((city) => {
+    return city.landArea < 1000;
+  });
+  createTableElements(areaLessThan, "allcities");
+  createTableElements([], "singlecity");
+});
+
+
+doesAnyCityButton.addEventListener("click", function () {
+  let anyCityLessThan = data.some((city) => {
+    return city.population < 100000;
+  });
+  if (anyCityLessThan) {
+    alert("Yes");
+  }
+  else {
+    alert("No");
+  }
+
+});
+
+doesEveryCityButton.addEventListener("click", function () {
+  let everyCityLand = data.every((city) => {
+    return city.population > 100;
+  });
+  if (everyCityLand) {
+    alert("Yes");
+  }
+  else {
+    alert("No");
+  }
+
+});
+
+
+//Select city
+
+const cityName = data.map(cityName => cityName.name); // Returns all city names with map
+console.log(cityName);
+
+//------------------------------------------------
+//Using for each to create option for every city
+
+cityName.forEach((element) => {
+  let cityCreate = document.createElement("option");
+  cityCreate.text = element;
+  cityCreate.value = element;
+  citySelect.appendChild(cityCreate);
+});
+
+// Found Item
+
+citySelect.addEventListener("change", (e) => {
+  const selectCities = data.filter(cities => e.target.value === cities.name);
+  createTableElements(selectCities, "singlecity");
+});
+
+
+
+
 
 
